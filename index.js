9ffefe8f55a9de02566a60aeb8f80a0de0749a71
AnuragA03 <anuragadhikari2003@gmail.com>
2023-10-06 20:12:24 +0530

    future improvement added

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value === ''){
+function addTask() {
+    if (inputBox.value === '') {
         alert("You must write something!");
     }
 
-    else{
+    else {
         let li = document.createElement("li")
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
@@ -18,13 +18,21 @@ function addTask(){
     inputBox.value = '';
 }
 
+//// i want to add the element using the button using enter keyword along with the existing ADD button
+
+// const addButton = document.getElementById("addButton");
+// addButton.addEventListener("keypress", function (event) {
+//     if (event.key === "Enter") {
+//         event.addTask();
+//     }
+// })
 
 // // this is important part for revision
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
     }
-    else if(e.target.tagName === "SPAN"){
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
     }
 }, false);
